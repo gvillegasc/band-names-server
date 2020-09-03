61c8ab3aae7abc1aa731006aefae4b6d16533c30
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-03 17:47:47 -0500

    Creating structure Bands

--- sockets/socket.js
+++ sockets/socket.js
@@ -10,4 +10,9 @@ io.on('connection', (client) => {
 		console.log('Mensaje', payload);
 		io.emit('mensaje', { admin: 'Nuevo mensaje' });
 	});
+
+	client.on('emitir-mensaje', (payload) => {
+		// io.emit('nuevo-mensaje', payload); //emite a todos;
+		client.broadcast.emit('nuevo-mensaje', payload);
+	});
 });
